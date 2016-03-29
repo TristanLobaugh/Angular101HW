8f9a8bbff0b2ecb6efd4d54fa048c2356b9f2320
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-29 00:16:44 -0400

    added angular manipulation
    made it so user can add to the array of objects using Angular

--- controller.js
+++ controller.js
@@ -2,68 +2,90 @@
 var myApp = angular.module("myApp", []);
 
 myApp.controller("myController", function($scope){
+	$scope.name = "";
+	$scope.population = "";
+	$scope.leader = "";
+	$scope.flagColors = "";
+
+	function Country(name, population, leader, flagColors){
+		this.name = name;
+		this.population = population;
+		this.leader = leader;
+		this.flagColors = flagColors;
+		$scope.countries.push(this);
+	}
 
 	$scope.countries = [
 		{
 			name: "USA",
-			pop: 1234500,
+			population: 1234500,
 			leader: "Obama",
 			flagColors: ["red", "white", "blue"]
 		},
 		{
 			name: "France",
-			pop: 2596278,
+			population: 2596278,
 			leader: "Francois Hollande",
 			flagColors: ["red", "white", "blue"]
 		},
 		{
 			name: "Italy",
-			pop: 78676876,
+			population: 78676876,
 			leader: "Sergio Mattarella",
 			flagColors: ["red", "white", "green"]
 		},
 		{
 			name: "Mexico",
-			pop: 87697868,
+			population: 87697868,
 			leader: "Enrique Pena Nieto",
 			flagColors: ["red", "white", "blue"]
 		},
 		{
 			name: "Argentina",
-			pop: 7869876897,
+			population: 7869876897,
 			leader: "Mauricio Macri",
 			flagColors: ["white", "blue"]
 		},
 		{
 			name: "Brazil",
-			pop: 8769867868,
+			population: 8769867868,
 			leader: "Dilma Rousseff",
 			flagColors: ["green", "yellow", "blue"]
 		},
 		{
 			name: "China",
-			pop: 8768968768796,
+			population: 8768968768796,
 			leader: "Xi Jiniping",
 			flagColors: ["red", "yellow"]
 		},
 		{
 			name: "Jamaica",
-			pop: 8769876,
+			population: 8769876,
 			leader: "Andrew Holness",
 			flagColors: ["green", "yellow", "black"]
 		},
 		{
 			name: "Japan",
-			pop: 876986,
+			population: 876986,
 			leader: "Shinzo Abe",
 			flagColors: ["red", "white"]
 		},
 		{
 			name: "Cook Islands",
-			pop: 87698,
+			population: 87698,
 			leader: "Henry Puna",
 			flagColors: ["red", "white", "blue"]
 		},
 	]
+
+	$scope.addToArray = function(){
+		if($scope.name.length > 0  && $scope.population.length > 0 && $scope.leader.length > 0 && $scope.flagColors.length > 0){
+			new Country($scope.name, $scope.population, $scope.leader, $scope.flagColors);
+			$scope.name = "";
+			$scope.population = "";
+			$scope.leader = "";
+			$scope.flagColors = "";
+		}
+	}
 });
 
